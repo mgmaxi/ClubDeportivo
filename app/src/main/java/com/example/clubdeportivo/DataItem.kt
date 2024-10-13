c38ccc72bfd69a484dd023a0ad0bcc75809c54e1
mgmaxi <maximiliano.ghirardi@gmail.com>
2024-10-13 20:27:14 -0300

    Add Membership search, Membership view, payment, expiration

---
+++ app/src/main/java/com/example/clubdeportivo/DataItem.kt
@@ -0,0 +1,9 @@
+package com.example.proyectoclubdeportivo
+
+data class DataItem (
+    val id: Long,
+    val nombre: String,
+    val dni: String,
+    val fechaVencimiento: String,
+    val importe: Double
+)
